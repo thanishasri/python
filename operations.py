1be56786c4cc33105eca18604d08c289fb224252
thanisha <thanishasri042@gmail.com>
2026-07-07 15:31:37 +0530

    day2

---
+++ operations.py
@@ -0,0 +1,120 @@
+# # product_price = 5000
+# # delivery_charges = 100
+# # total = product_price + delivery_charges
+# # print(total)
+
+# # #####
+# # a=10
+# # b=3
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a/b)
+# # print(a//b)
+# # print(a%b)
+# # print(a**b)
+
+# # student = 10
+# # group = 2
+# # print(student // group)
+
+# # ######
+# # followers = 100
+# # followers = followers + 1
+# # print(followers)
+
+# # ######
+# # # saved_password = "1234"
+# # # entered_password = "1234"
+# # # print(saved_password == entered_password)
+
+# # # ####
+
+# # # balance = 200
+# # pin_correct = True
+# # if balance >= 1000 and pin_correct:
+# #     print("withdraw allowed")
+# # else:
+# #     print("failed")
+    
+# #     #########
+# # a=int(input("enter first number"))
+# # operator= (input("enter operator"))
+# # b=int(input("enter second number"))
+# if operator =="+":
+# #     print(a+b)
+# # if operator =="*":
+# #     print(a*b)
+
+# # if operator =="/":
+# #     print(a/b)
+    
+# # else:
+# #     print("invalid operator")
+
+# # product = input("enter product name")
+# # price = int(input("enter the price"))
+# # quantity=int(input("enter the quantity"))
+# # discount=int(input("enter discount amount"))
+# # total = price * quantity
+# # final_bill = total-discount
+# # print("product:", product)
+# # print("total amount:",total)
+# # print("final bill:",final_bill)
+
+# # ##########
+# # password = input ("enter the password:")
+# # if password =="admin123":
+# #         print("welcome")
+# # else:
+# #         print("wrong password")
+        
+#         #######
+        
+# # age =20
+# # if age >=18:
+# # #     print("eligible to vote")
+    
+# # marks= int(input("enter the marks"))
+# # if marks >= 90:
+# #     print("A grade")
+# # elif marks >=75:
+# #     print("B grade")
+# # elif marks >=50:
+# #     print("C grade")
+# # else:
+# #     print("fail")
+
+
+# #send mail
+# #print("sendmail")
+
+
+# for i in range(5):
+#     print("send mail")
+# users=["taruni","thanisha"]
+# for users in users:
+#     print("message sent to you",users)
+    
+# name = "hello"
+# for ch in name:
+#     print(ch)
+    
+count=1
+while count>=5:
+    print(count)
+    count +=3
+ 
+for  in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+password= ""
+while password != "1234":
+    password = input("enter password")
+    print("login success")
+    
+student1 = "thanisha"
+student2 = "thskj"
+student3 
